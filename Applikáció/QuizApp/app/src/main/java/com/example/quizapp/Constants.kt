3b1a1a509ad915c433bd280ec51c25726642ce90
trkszabina <72131659+trkszabina@users.noreply.github.com>
2022-11-06 17:11:34 +0200

    Constants.kt hozzáadásra került

---
+++ Applikáció/QuizApp/app/src/main/java/com/example/quizapp/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.quizapp
+
+object Constants {
+    const val USER_NAME: String = "user_name"
+    const val TOTAL_QUESTIONS: String = "total_question"
+    const val CORRECT_ANSWERS: String = "correct_answers"
+}
