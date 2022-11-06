20e7991cb82409a4e74996e4e47b459d3befc870
snupierik2001 <44303033+snupierik2001@users.noreply.github.com>
2022-11-06 22:34:04 +0100

    Quiz Activity funkció hozzáadása

--- Applikáció/QuizApp/app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
+++ Applikáció/QuizApp/app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
@@ -3,11 +3,25 @@ package com.example.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
 
-        val QuestionList = Constants.getQuestions()
+        val questionList = Constants.getQuestions()
+
+        val currentPosition = 1
+        val question: Question? = questionList[currentPosition -1]
+
+        progressBar.progress = currentPosition
+        tv_progress.text = "$currentPosition" + "/" + progressBar.max
+
+        tv_question.text = question!!.question
+        iv_image.setImageResource(question.image)
+        tv_option_one.text = question.OptionOne
+        tv_option_two.text = question.OptionTwo
+        tv_option_three.text = question.OptionThree
+        tv_option_four.text = question.OptionFour
     }
 }
