f79069c7f107e95a0bdde81fc9265c3a3ff9fe19
trkszabina <72131659+trkszabina@users.noreply.github.com>
2022-11-06 17:15:58 +0200

    MainActivity frissítésre került

--- Applikáció/QuizApp/app/src/main/java/com/example/quizapp/MainActivity.kt
+++ Applikáció/QuizApp/app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.quizapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.toTheResults.setOnClickListener{
+            startActivity(Intent(this@MainActivity, ResultActivity::class.java))
+        }
     }
 }
