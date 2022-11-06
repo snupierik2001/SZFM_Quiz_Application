894ba2ca4f6299ca5719a44358425176ae39cb77
snupierik2001 <44303033+snupierik2001@users.noreply.github.com>
2022-11-06 21:13:40 +0100

    Első kérdés hozzáadása

--- Applikáció/QuizApp/app/src/main/java/com/example/quizapp/Constants.kt
+++ Applikáció/QuizApp/app/src/main/java/com/example/quizapp/Constants.kt
@@ -4,4 +4,19 @@ object Constants {
     const val USER_NAME: String = "user_name"
     const val TOTAL_QUESTIONS: String = "total_question"
     const val CORRECT_ANSWERS: String = "correct_answers"
+
+    fun getQuestions(): ArrayList<Question> {
+        val QuestionList = ArrayList<Question>()
+
+        val QuestionOne = Question(1, "Milyen programozási nyelvnek ez a ikonja?",
+        R.drawable.javaIkon,
+            "Java",
+            "Phyton",
+            "Oracle",
+            "JavaScript",
+            1
+        )
+        QuestionList.add(QuestionOne)
+        return QuestionList
+    }
 }
