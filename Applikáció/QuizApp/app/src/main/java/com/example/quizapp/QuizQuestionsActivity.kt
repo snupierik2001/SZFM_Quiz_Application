1cae9c50bdafca598c9eabe57cc52ef7d11259e6
snupierik2001 <44303033+snupierik2001@users.noreply.github.com>
2022-11-06 23:16:37 +0100

    Quiz Question Activity funkció hozzáadása

--- Applikáció/QuizApp/app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
+++ Applikáció/QuizApp/app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
@@ -1,21 +1,46 @@
 package com.example.quizapp
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.service.autofill.OnClickAction
+import android.view.View
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 
 
-class QuizQuestionsActivity : AppCompatActivity() {
+class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var mCurrentPosition:Int = 1
+    private var mQuestionList: ArrayList<Question>? = null
+    private var mSelectedOptionPosition : Int = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
 
-        val questionList = Constants.getQuestions()
+        mQuestionList = Constants.getQuestions()
 
-        val currentPosition = 1
-        val question: Question? = questionList[currentPosition -1]
+        setsQuestion()
 
-        progressBar.progress = currentPosition
-        tv_progress.text = "$currentPosition" + "/" + progressBar.max
+        tv_option_one.setOnclickListener(this)
+        tv_option_two.setOnclickListener(this)
+        tv_option_three.setOnclickListener(this)
+        tv_option_four.setOnclickListener(this)
+
+
+    }
+
+    private  fun setsQuestion(){
+        mCurrentPosition = 1
+        val question = mQuestionList!![mCurrentPosition-1]
+
+        defaultOptionsView()
+
+        progressBar.progress = mCurrentPosition
+        tv_progress.text = "$mCurrentPosition" + "/" + progressBar.max
 
         tv_question.text = question!!.question
         iv_image.setImageResource(question.image)
@@ -24,4 +49,51 @@ class QuizQuestionsActivity : AppCompatActivity() {
         tv_option_three.text = question.OptionThree
         tv_option_four.text = question.OptionFour
     }
+
+    private fun defaultOptionsView(){
+        val options = ArrayList<TextView>()
+        options.add(0, tv_option_one)
+        options.add(1, tv_option_two)
+        options.add(2, tv_option_three)
+        options.add(3, tv_option_four)
+
+        for (option in options){
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(
+                this,
+                R.drawable.default_option_border_bg
+            )
+        }
+    }
+
+    override fun onClick (v: View?) {
+        when(v?.id){
+            R.id.tv_option_one ->{
+                selectedOptionView(tv_option_one,1)
+            }
+            R.id.tv_option_two ->{
+                selectedOptionView(tv_option_two,2)
+            }
+            R.id.tv_option_three ->{
+                selectedOptionView(tv_option_three,3)
+            }
+            R.id.tv_option_four ->{
+                selectedOptionView(tv_option_four,4)
+            }
+}
+    }
+
+    private fun selectedOptionView(tv: TextView, selectedOptionNum: Int ){
+        defaultOptionsView()
+        mSelectedOptionPosition = selectedOptionNum
+        tv.setTextColor(Color.parseColor("#363A43"))
+        tv.typeface = Typeface.DEFAULT
+        tv.background = ContextCompat.getDrawable(
+            this,
+            R.drawable.default_option_border_bg
+        )
+    }
+
+
 }
